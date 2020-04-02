ec7c83734125ab2361dca423fbf5dbb1b7a7f414
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2020-04-02 12:36:34 -0500

    cambio a spring

---
+++ src/test/java/edu/escuelaing/arep/principal/Laboratorio08ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arep.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
