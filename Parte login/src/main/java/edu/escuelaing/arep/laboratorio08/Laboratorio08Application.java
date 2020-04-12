4fe4a9870e8b5a142c79bbd56bffe11e5c199c84
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2020-04-11 19:35:17 -0500

    login iniciado

---
+++ Parte login/src/main/java/edu/escuelaing/arep/laboratorio08/Laboratorio08Application.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arep.laboratorio08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio08Application.class, args);
+	}
+
+}
---
+++ Parte login/src/test/java/edu/escuelaing/arep/laboratorio08/Laboratorio08ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arep.laboratorio08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
